bfb66969baaa38a16e9dfce872e6ad6a4012d443
Ben Christensen <benjchristensen@netflix.com>
2012-12-04 13:21:40 -0800

    Remove Strategy Injection on HystrixCommand
    https://github.com/Netflix/Hystrix/issues/34

--- hystrix-core/src/main/java/com/netflix/hystrix/strategy/HystrixPlugins.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/strategy/HystrixPlugins.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2012 Netflix, Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,6 @@
  */
 package com.netflix.hystrix.strategy;
 
-import com.netflix.hystrix.HystrixCollapser;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategyDefault;
@@ -30,14 +29,8 @@ import com.netflix.hystrix.strategy.properties.HystrixPropertiesStrategyDefault;
  * Registry for plugin implementations that allows global override and handles the retrieval of correct implementation based on order of precedence:
  * <ol>
  * <li>plugin registered globally via <code>register</code> methods in this class</li>
- * <li>injected via {@link HystrixCommand} and {@link HystrixCollapser} constructors</li>
  * <li>default implementations</li>
  * </ol>
- * <p>
- * The injection approach is effective for {@link HystrixCommand} and {@link HystrixCollapser} implementations where you wish to have a different implementation without
- * overriding all implementations. It is also useful when distributing a library where static override should not be used.
- * <p>
- * The globally registered plugin is useful when using commands from 3rd party libraries and you want to override all implementations in your entire system.
  */
 public class HystrixPlugins {
 
@@ -63,18 +56,13 @@ public class HystrixPlugins {
      *            {@link HystrixEventNotifier} implementation as injected via {@link HystrixCommand}
      * @return {@link HystrixEventNotifier} implementation to use
      */
-    public HystrixEventNotifier getEventNotifier(HystrixEventNotifier injected) {
+    public HystrixEventNotifier getEventNotifier() {
         if (notifier != null) {
             // we have a global override so use it
             return notifier;
         } else {
-            if (injected != null) {
-                // we have an injected default
-                return injected;
-            } else {
-                // we don't have an injected default nor an override so construct a default
-                return HystrixEventNotifierDefault.getInstance();
-            }
+            // we don't have an injected default nor an override so construct a default
+            return HystrixEventNotifierDefault.getInstance();
         }
     }
 
@@ -102,18 +90,13 @@ public class HystrixPlugins {
      *            {@link HystrixConcurrencyStrategy} implementation as injected via {@link HystrixCommand}
      * @return {@link HystrixConcurrencyStrategy} implementation to use
      */
-    public HystrixConcurrencyStrategy getConcurrencyStrategy(HystrixConcurrencyStrategy injected) {
+    public HystrixConcurrencyStrategy getConcurrencyStrategy() {
         if (concurrencyStrategy != null) {
             // we have a global override so use it
             return concurrencyStrategy;
         } else {
-            if (injected != null) {
-                // we have an injected default
-                return injected;
-            } else {
-                // we don't have an injected default nor an override so construct a default
-                return HystrixConcurrencyStrategyDefault.getInstance();
-            }
+            // we don't have an injected default nor an override so construct a default
+            return HystrixConcurrencyStrategyDefault.getInstance();
         }
     }
 
@@ -134,18 +117,13 @@ public class HystrixPlugins {
      *            {@link HystrixMetricsPublisher} implementation as injected via {@link HystrixCommand}
      * @return {@link HystrixMetricsPublisher} implementation to use
      */
-    public HystrixMetricsPublisher getMetricsPublisher(HystrixMetricsPublisher injected) {
+    public HystrixMetricsPublisher getMetricsPublisher() {
         if (metricsPublisher != null) {
             // we have a global override so use it
             return metricsPublisher;
         } else {
-            if (injected != null) {
-                // we have an injected default
-                return injected;
-            } else {
-                // we don't have an injected default nor an override so construct a default
-                return HystrixMetricsPublisherDefault.getInstance();
-            }
+            // we don't have an injected default nor an override so construct a default
+            return HystrixMetricsPublisherDefault.getInstance();
         }
     }
 
@@ -166,18 +144,13 @@ public class HystrixPlugins {
      *            {@link HystrixPropertiesStrategy} implementation as injected via {@link HystrixCommand}
      * @return {@link HystrixPropertiesStrategy} implementation to use
      */
-    public HystrixPropertiesStrategy getPropertiesStrategy(HystrixPropertiesStrategy injected) {
+    public HystrixPropertiesStrategy getPropertiesStrategy() {
         if (propertiesFactory != null) {
             // we have a global override so use it
             return propertiesFactory;
         } else {
-            if (injected != null) {
-                // we have an injected default
-                return injected;
-            } else {
-                // we don't have an injected default nor an override so construct a default
-                return HystrixPropertiesStrategyDefault.getInstance();
-            }
+            // we don't have an injected default nor an override so construct a default
+            return HystrixPropertiesStrategyDefault.getInstance();
         }
     }
 
