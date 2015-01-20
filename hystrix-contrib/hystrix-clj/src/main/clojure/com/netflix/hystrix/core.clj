00aa1d38a21394811e8ab555535a7a3e01d4b10a
Matt Jacobs <mjacobs@netflix.com>
2015-01-20 12:10:31 -0800

    s/observeOne/observeOn

--- hystrix-contrib/hystrix-clj/src/main/clojure/com/netflix/hystrix/core.clj
+++ hystrix-contrib/hystrix-clj/src/main/clojure/com/netflix/hystrix/core.clj
@@ -637,7 +637,7 @@
     (observe-later-on* [this scheduler] (.observeOn (.toObservable this) scheduler))
   HystrixCollapser
     (observe-later* [this] (.toObservable this))
-    (observe-later-on* [this scheduler] (.observeOne (.toObservable this) scheduler)))
+    (observe-later-on* [this scheduler] (.observeOn (.toObservable this) scheduler)))
 
 (defn observe-later
   "Same as #'com.netflix.hystrix.core/observe, but command execution does not begin until the
