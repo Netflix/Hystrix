4bc067c5bf8e3cce3d4230ece97374da5b6eb615
Matt Jacobs <mjacobs@netflix.com>
2015-01-27 22:40:18 -0800

    Fix references to deprecated methods

--- hystrix-core/src/main/java/com/netflix/hystrix/HystrixCollapser.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/HystrixCollapser.java
@@ -378,7 +378,7 @@ public abstract class HystrixCollapser<BatchReturnType, ResponseType, RequestArg
     public Observable<ResponseType> toObservable(Scheduler observeOn) {
 
         /* try from cache first */
-        if (getProperties().requestCachingEnabled().get()) {
+        if (getProperties().requestCacheEnabled().get()) {
             Observable<ResponseType> fromCache = requestCache.get(getCacheKey());
             if (fromCache != null) {
                 metrics.markResponseFromCache();
@@ -389,7 +389,7 @@ public abstract class HystrixCollapser<BatchReturnType, ResponseType, RequestArg
         RequestCollapser<BatchReturnType, ResponseType, RequestArgumentType> requestCollapser = collapserFactory.getRequestCollapser(collapserInstanceWrapper);
         Observable<ResponseType> response = requestCollapser.submitRequest(getRequestArgument());
         metrics.markRequestBatched();
-        if (getProperties().requestCachingEnabled().get()) {
+        if (getProperties().requestCacheEnabled().get()) {
             /*
              * A race can occur here with multiple threads queuing but only one will be cached.
              * This means we can have some duplication of requests in a thread-race but we're okay
--- hystrix-core/src/main/java/com/netflix/hystrix/HystrixObservableCollapser.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/HystrixObservableCollapser.java
@@ -370,7 +370,7 @@ public abstract class HystrixObservableCollapser<K, BatchReturnType, ResponseTyp
     public Observable<ResponseType> toObservable(Scheduler observeOn) {
 
         /* try from cache first */
-        if (getProperties().requestCachingEnabled().get()) {
+        if (getProperties().requestCacheEnabled().get()) {
             Observable<ResponseType> fromCache = requestCache.get(getCacheKey());
             if (fromCache != null) {
                 /* mark that we received this response from cache */
@@ -384,7 +384,7 @@ public abstract class HystrixObservableCollapser<K, BatchReturnType, ResponseTyp
 
         RequestCollapser<BatchReturnType, ResponseType, RequestArgumentType> requestCollapser = collapserFactory.getRequestCollapser(collapserInstanceWrapper);
         Observable<ResponseType> response = requestCollapser.submitRequest(getRequestArgument());
-        if (getProperties().requestCachingEnabled().get()) {
+        if (getProperties().requestCacheEnabled().get()) {
             /*
              * A race can occur here with multiple threads queuing but only one will be cached.
              * This means we can have some duplication of requests in a thread-race but we're okay
