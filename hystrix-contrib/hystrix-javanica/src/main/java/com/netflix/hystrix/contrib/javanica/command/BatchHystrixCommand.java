a614fdf9e2f3ad4e178db8253cd6a79be10bb417
dmgcodevil <dmgcodevil@gmail.com>
2015-03-29 17:35:32 +0300

    #689: revert to java 6

--- hystrix-contrib/hystrix-javanica/src/main/java/com/netflix/hystrix/contrib/javanica/command/BatchHystrixCommand.java
+++ hystrix-contrib/hystrix-javanica/src/main/java/com/netflix/hystrix/contrib/javanica/command/BatchHystrixCommand.java
@@ -86,7 +86,7 @@ public class BatchHystrixCommand extends AbstractHystrixCommand<List<Object>> {
     }
 
     private List<Object> collect(Collection<HystrixCollapser.CollapsedRequest<Object, Object>> requests) {
-        List<Object> commandArgs = new ArrayList<>();
+        List<Object> commandArgs = new ArrayList<Object>();
         for (HystrixCollapser.CollapsedRequest<Object, Object> request : requests) {
             final Object[] args = (Object[]) request.getArgument();
             commandArgs.add(args[0]);
