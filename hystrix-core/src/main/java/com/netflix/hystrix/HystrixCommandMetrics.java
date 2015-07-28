795e937d8be8e449f28d04b74c21370b0f5dd3f5
Matt Jacobs <mjacobs@netflix.com>
2015-07-28 10:14:23 -0700

    Made HystrixCommandMetrics.getHealthCounts() final

--- hystrix-core/src/main/java/com/netflix/hystrix/HystrixCommandMetrics.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/HystrixCommandMetrics.java
@@ -408,8 +408,7 @@ public abstract class HystrixCommandMetrics extends HystrixMetrics {
      * 
      * @return {@link HealthCounts}
      */
-    //TODO Should this be final?
-    public HealthCounts getHealthCounts() {
+    public final HealthCounts getHealthCounts() {
         // we put an interval between snapshots so high-volume commands don't 
         // spend too much unnecessary time calculating metrics in very small time periods
         long lastTime = lastHealthCountsSnapshot.get();
