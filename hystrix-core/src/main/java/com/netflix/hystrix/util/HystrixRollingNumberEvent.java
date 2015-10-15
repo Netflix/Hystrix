5a5202bdf88929d31fbdb117340cb2cbe094851c
Matt Jacobs <mjacobs@netflix.com>
2015-10-15 16:47:43 -0700

    Refactor HystrixYammerMetricsPublisherCommand to allow subclasses to be less-aware of metrics internals

--- hystrix-core/src/main/java/com/netflix/hystrix/util/HystrixRollingNumberEvent.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/util/HystrixRollingNumberEvent.java
@@ -15,6 +15,8 @@
  */
 package com.netflix.hystrix.util;
 
+import com.netflix.hystrix.HystrixEventType;
+
 /**
  * Various states/events that can be captured in the {@link HystrixRollingNumber}.
  * <p>
@@ -46,5 +48,27 @@ public enum HystrixRollingNumberEvent {
 
     public boolean isMaxUpdater() {
         return type == 2;
+    }
+
+    public static HystrixRollingNumberEvent from(HystrixEventType eventType) {
+        switch (eventType) {
+            case BAD_REQUEST: return HystrixRollingNumberEvent.BAD_REQUEST;
+            case COLLAPSED: return HystrixRollingNumberEvent.COLLAPSED;
+            case EMIT: return HystrixRollingNumberEvent.EMIT;
+            case EXCEPTION_THROWN: return HystrixRollingNumberEvent.EXCEPTION_THROWN;
+            case FAILURE: return HystrixRollingNumberEvent.FAILURE;
+            case FALLBACK_EMIT: return HystrixRollingNumberEvent.FALLBACK_EMIT;
+            case FALLBACK_FAILURE: return HystrixRollingNumberEvent.FALLBACK_FAILURE;
+            case FALLBACK_MISSING: return HystrixRollingNumberEvent.FALLBACK_MISSING;
+            case FALLBACK_REJECTION: return HystrixRollingNumberEvent.FALLBACK_REJECTION;
+            case FALLBACK_SUCCESS: return HystrixRollingNumberEvent.FALLBACK_SUCCESS;
+            case RESPONSE_FROM_CACHE: return HystrixRollingNumberEvent.RESPONSE_FROM_CACHE;
+            case SEMAPHORE_REJECTED: return HystrixRollingNumberEvent.SEMAPHORE_REJECTED;
+            case SHORT_CIRCUITED: return HystrixRollingNumberEvent.SHORT_CIRCUITED;
+            case SUCCESS: return HystrixRollingNumberEvent.SUCCESS;
+            case THREAD_POOL_REJECTED: return HystrixRollingNumberEvent.THREAD_POOL_REJECTED;
+            case TIMEOUT: return HystrixRollingNumberEvent.TIMEOUT;
+            default: throw new RuntimeException("Unknown HystrixEventType : " + eventType);
+        }
     }
 }
