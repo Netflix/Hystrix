fd40c7ee20b547573193ef7bf80625a991e04a05
Matt Jacobs <mjacobs@netflix.com>
2015-11-10 14:15:38 -0800

    Added defaultSetter() methods to properties classes as a workaround for GROOVY-6286

--- hystrix-core/src/main/java/com/netflix/hystrix/HystrixCollapserProperties.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/HystrixCollapserProperties.java
@@ -191,6 +191,15 @@ public abstract class HystrixCollapserProperties {
     }
 
     /**
+     * Factory method to retrieve the default Setter.
+     * Groovy has a bug (GROOVY-6286) which does not allow method names and inner classes to have the same name
+     * This method fixes Issue #967 and allows Groovy consumers to choose this method and not trigger the bug
+     */
+    public static Setter defaultSetter() {
+        return Setter();
+    }
+
+    /**
      * Fluent interface that allows chained setting of properties that can be passed into a {@link HystrixCollapser} constructor to inject instance specific property overrides.
      * <p>
      * See {@link HystrixPropertiesStrategy} for more information on order of precedence.
