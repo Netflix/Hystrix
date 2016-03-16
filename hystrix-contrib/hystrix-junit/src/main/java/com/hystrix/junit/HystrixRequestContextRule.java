fbb6e13c24352e25c67926a20ea3d4bddfab4799
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-03-15 22:22:49 -0300

    Proposal: HystrixRequestContext junit rule
    This will make testing commands that requires a `HystrixRequestContext`
    easier in JUnit by using its Rule feature.
    
    An example of usage would be something like:
    
    ```java
        @Rule
        public HystrixRequestContextRule request = new HystrixRequestContextRule();
    
        @Test
        public void initsContext() {
            MatcherAssert.assertThat(this.request.context(), CoreMatchers.notNullValue());
        }
    ```
    
    I put it in the `hystrix-contrib` folder, let me know if this is the right thing to do, please.

---
+++ hystrix-contrib/hystrix-junit/src/main/java/com/hystrix/junit/HystrixRequestContextRule.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2016 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hystrix.junit;
+
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
+import org.junit.rules.ExternalResource;
+
+/**
+ * JUnit rule to be used to simplify tests that require a HystrixRequestContext.
+ *
+ * Example of usage:
+ *
+ * <blockquote>
+ * <pre>
+ *     @Rule
+ *     public HystrixRequestContextRule context = new HystrixRequestContextRule();
+ * </pre>
+ * </blockquote>
+ *
+ */
+public final class HystrixRequestContextRule extends ExternalResource {
+    private HystrixRequestContext context;
+
+    @Override
+    protected void before() throws Throwable {
+        this.context = HystrixRequestContext.initializeContext();
+    }
+
+    @Override
+    protected void after() {
+        if (this.context != null) {
+            this.context.shutdown();
+        }
+    }
+
+    public HystrixRequestContext context() {
+        return this.context;
+    }
+}
---
+++ hystrix-contrib/hystrix-junit/src/test/java/com/hystrix/junit/HystrixRequestContextRuleTest.java
@@ -0,0 +1,21 @@
+package com.hystrix.junit;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.Rule;
+import org.junit.Test;
+
+public final class HystrixRequestContextRuleTest {
+    @Rule
+    public HystrixRequestContextRule request = new HystrixRequestContextRule();
+
+    @Test
+    public void initsContext() {
+        MatcherAssert.assertThat(this.request.context(), CoreMatchers.notNullValue());
+    }
+
+    @Test
+    public void manuallyShutdownContextDontBreak() {
+        this.request.after();
+    }
+}
