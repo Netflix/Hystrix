6fde60f50290aa526e6fe9d8dd353915c9e7709d
Matt Jacobs <mjacobs@netflix.com>
2016-04-07 11:43:54 -0700

    Removing extraneous printlns

--- hystrix-core/src/main/java/com/netflix/hystrix/metric/CachedValuesHistogram.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/metric/CachedValuesHistogram.java
@@ -50,7 +50,6 @@ public class CachedValuesHistogram {
     private final int p100;
 
     public static CachedValuesHistogram backedBy(Histogram underlying) {
-        //System.out.println("^^^ " + underlying.hashCode() + " : backedBy : " + Thread.currentThread().getName());
         return new CachedValuesHistogram(underlying);
     }
 
@@ -93,7 +92,6 @@ public class CachedValuesHistogram {
     }
 
     public Histogram getUnderlying() {
-        //System.out.println("^^^ " + underlying.hashCode() + " : getUnderlying : " + Thread.currentThread().getName());
         return underlying;
     }
 
