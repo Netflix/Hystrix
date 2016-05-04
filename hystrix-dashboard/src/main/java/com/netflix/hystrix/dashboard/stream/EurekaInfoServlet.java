fe82af9866e9fa3a8b7305ec5f19fb5229d754fb
Diego Pacheco <diego.pachecoit@gmail.com>
2016-05-04 11:46:35 -0300

    added license

--- hystrix-dashboard/src/main/java/com/netflix/hystrix/dashboard/stream/EurekaInfoServlet.java
+++ hystrix-dashboard/src/main/java/com/netflix/hystrix/dashboard/stream/EurekaInfoServlet.java
@@ -10,6 +10,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 
 /**
+ * Copyright 2013 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Servlet that calls eureka REST api in order to get instances information. <BR>
  * You need provide a url parameter. i.e: eureka?url=http://127.0.0.1:8080/eureka/v2/apps
  * 
