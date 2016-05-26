be95845d34ee018a3268d41d0113a50025fc8486
Zach A. Thomas <zach.thomas@inin.com>
2016-05-26 15:36:27 -0400

    improve usages of "its" and "it's"

--- hystrix-contrib/hystrix-javanica/src/main/java/com/netflix/hystrix/contrib/javanica/utils/FallbackMethod.java
+++ hystrix-contrib/hystrix-javanica/src/main/java/com/netflix/hystrix/contrib/javanica/utils/FallbackMethod.java
@@ -142,14 +142,14 @@ public class FallbackMethod {
     private void validatePlainReturnType(Class<?> commandReturnType, Class<?> fallbackReturnType, Method commandMethod, Method fallbackMethod) {
         if (!commandReturnType.isAssignableFrom(fallbackReturnType)) {
             throw new FallbackDefinitionException(createErrorMsg(commandMethod, fallbackMethod, "Fallback method '"
-                    + fallbackMethod + "' must return: " + commandReturnType + " or it's subclass"));
+                    + fallbackMethod + "' must return: " + commandReturnType + " or its subclass"));
         }
     }
 
     private void validateParametrizedType(Type commandReturnType, Type fallbackReturnType, Method commandMethod, Method fallbackMethod) {
         if (!commandReturnType.equals(fallbackReturnType)) {
             throw new FallbackDefinitionException(createErrorMsg(commandMethod, fallbackMethod, "Fallback method '"
-                    + fallbackMethod + "' must return: " + commandReturnType + " or it's subclass"));
+                    + fallbackMethod + "' must return: " + commandReturnType + " or its subclass"));
         }
     }
 
