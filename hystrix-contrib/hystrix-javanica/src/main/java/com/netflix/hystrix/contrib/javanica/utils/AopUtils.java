cb2767fe02a1cedf5296d6e45a72b596c5e21b39
dmgcodevil <dmgcodevil@gmail.com>
2017-02-20 17:38:15 -0500

    iss1458: added sanity checker for the method that detects point cut type by method annotations

--- hystrix-contrib/hystrix-javanica/src/main/java/com/netflix/hystrix/contrib/javanica/utils/AopUtils.java
+++ hystrix-contrib/hystrix-javanica/src/main/java/com/netflix/hystrix/contrib/javanica/utils/AopUtils.java
@@ -24,6 +24,8 @@ import org.aspectj.lang.reflect.MethodSignature;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 
 /**
  * Provides common methods to retrieve information from JoinPoint and not only.
@@ -121,6 +123,78 @@ public final class AopUtils {
         }
 
         return Optional.absent();
+    }
+
+    public static String getMethodInfo(Method m) {
+        StringBuilder info = new StringBuilder();
+        info.append("Method signature:").append("\n");
+        info.append(m.toGenericString()).append("\n");
+
+        info.append("\nFlags:").append("\n");
+        info.append("Bridge=").append(m.isBridge()).append("\n");
+        info.append("Synthetic=").append(m.isSynthetic()).append("\n");
+        info.append("Final=").append(Modifier.isFinal(m.getModifiers())).append("\n");
+        info.append("Native=").append(Modifier.isNative(m.getModifiers())).append("\n");
+        info.append("Synchronized=").append(Modifier.isSynchronized(m.getModifiers())).append("\n");
+        info.append("Abstract=").append(Modifier.isAbstract(m.getModifiers())).append("\n");
+        info.append("AccessLevel=").append(getAccessLevel(m.getModifiers())).append("\n");
+
+        info.append("\nReturn Type: \n");
+        info.append("ReturnType=").append(m.getReturnType()).append("\n");
+        info.append("GenericReturnType=").append(m.getGenericReturnType()).append("\n");
+
+        info.append("\nParameters:");
+        Class<?>[] pType = m.getParameterTypes();
+        Type[] gpType = m.getGenericParameterTypes();
+        if (pType.length != 0) {
+            info.append("\n");
+        } else {
+            info.append("empty\n");
+        }
+        for (int i = 0; i < pType.length; i++) {
+            info.append("parameter [").append(i).append("]:\n");
+            info.append("ParameterType=").append(pType[i]).append("\n");
+            info.append("GenericParameterType=").append(gpType[i]).append("\n");
+        }
+
+        info.append("\nExceptions:");
+        Class<?>[] xType = m.getExceptionTypes();
+        Type[] gxType = m.getGenericExceptionTypes();
+        if (xType.length != 0) {
+            info.append("\n");
+        } else {
+            info.append("empty\n");
+        }
+        for (int i = 0; i < xType.length; i++) {
+            info.append("exception [").append(i).append("]:\n");
+            info.append("ExceptionType=").append(xType[i]).append("\n");
+            info.append("GenericExceptionType=").append(gxType[i]).append("\n");
+        }
+
+        info.append("\nAnnotations:");
+        if (m.getAnnotations().length != 0) {
+            info.append("\n");
+        } else {
+            info.append("empty\n");
+        }
+
+        for (int i = 0; i < m.getAnnotations().length; i++) {
+            info.append("annotation[").append(i).append("]=").append(m.getAnnotations()[i]).append("\n");
+        }
+
+        return info.toString();
+    }
+
+    private static String getAccessLevel(int modifiers) {
+        if (Modifier.isPublic(modifiers)) {
+            return "public";
+        } else if (Modifier.isProtected(modifiers)) {
+            return "protected";
+        } else if (Modifier.isPrivate(modifiers)) {
+            return "private";
+        } else {
+            return "default";
+        }
     }
 
 }
