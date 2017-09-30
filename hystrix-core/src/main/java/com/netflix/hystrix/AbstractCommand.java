5b3c6fdd27b6196ec31153c0273a01b1454adcd2
Alexandre S <alexandre.strevenski@gmail.com>
2017-09-30 01:07:20 -0300

    just removed duplicated code

--- hystrix-core/src/main/java/com/netflix/hystrix/AbstractCommand.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/AbstractCommand.java
@@ -384,29 +384,24 @@ import java.util.concurrent.atomic.AtomicReference;
             public void call() {
                 circuitBreaker.markNonSuccess();
                 if (_cmd.commandState.compareAndSet(CommandState.OBSERVABLE_CHAIN_CREATED, CommandState.UNSUBSCRIBED)) {
-                    if (!_cmd.executionResult.containsTerminalEvent()) {
-                        _cmd.eventNotifier.markEvent(HystrixEventType.CANCELLED, _cmd.commandKey);
-                        try {
-                            executionHook.onUnsubscribe(_cmd);
-                        } catch (Throwable hookEx) {
-                            logger.warn("Error calling HystrixCommandExecutionHook.onUnsubscribe", hookEx);
-                        }
-                        _cmd.executionResultAtTimeOfCancellation = _cmd.executionResult
-                                .addEvent((int) (System.currentTimeMillis() - _cmd.commandStartTimestamp), HystrixEventType.CANCELLED);
-                    }
+                    cancelAndUnsubscribe();
                     handleCommandEnd(false); //user code never ran
                 } else if (_cmd.commandState.compareAndSet(CommandState.USER_CODE_EXECUTED, CommandState.UNSUBSCRIBED)) {
-                    if (!_cmd.executionResult.containsTerminalEvent()) {
-                        _cmd.eventNotifier.markEvent(HystrixEventType.CANCELLED, _cmd.commandKey);
-                        try {
-                            executionHook.onUnsubscribe(_cmd);
-                        } catch (Throwable hookEx) {
-                            logger.warn("Error calling HystrixCommandExecutionHook.onUnsubscribe", hookEx);
-                        }
-                        _cmd.executionResultAtTimeOfCancellation = _cmd.executionResult
-                                .addEvent((int) (System.currentTimeMillis() - _cmd.commandStartTimestamp), HystrixEventType.CANCELLED);
-                    }
+                    cancelAndUnsubscribe();
                     handleCommandEnd(true); //user code did run
+                }
+            }
+
+            private void cancelAndUnsubscribe() {
+                if (!_cmd.executionResult.containsTerminalEvent()) {
+                    _cmd.eventNotifier.markEvent(HystrixEventType.CANCELLED, _cmd.commandKey);
+                    try {
+                        executionHook.onUnsubscribe(_cmd);
+                    } catch (Throwable hookEx) {
+                        logger.warn("Error calling HystrixCommandExecutionHook.onUnsubscribe", hookEx);
+                    }
+                    _cmd.executionResultAtTimeOfCancellation = _cmd.executionResult
+                            .addEvent((int) (System.currentTimeMillis() - _cmd.commandStartTimestamp), HystrixEventType.CANCELLED);
                 }
             }
         };
