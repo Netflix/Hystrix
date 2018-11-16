ce37c0425bd46aa1835757f1e17b29045b5eab36
elandau <elandau@netflix.com>
2018-11-16 12:31:20 -0800

    Ignore tests for all builds

--- installViaTravis.sh
+++ installViaTravis.sh
@@ -3,14 +3,14 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Assemble Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew assemble
+  ./gradlew assemble -x test
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Assemble Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew -Prelease.travisci=true assemble
+  ./gradlew -Prelease.travisci=true assemble -x test
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Assemble Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true assemble
+  ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true assemble -x test
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew assemble
+  ./gradlew assemble -x test
 fi
